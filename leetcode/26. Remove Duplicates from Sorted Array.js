c8f7664b1c9a0bc33bb6c8727f98d66bcb2d232d
SleeplessN <gmlwns7020@naver.com>
2022-05-27 18:24:47 +0900

    Add url

--- leetcode/26. Remove Duplicates from Sorted Array.js
+++ leetcode/26. Remove Duplicates from Sorted Array.js
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
 var solution = function (nums) {
   for (let i = 0; i < nums.length - 1; i++) {
     if (nums[i] === nums[i + 1]) {
@@ -7,17 +9,3 @@ var solution = function (nums) {
   }
   return nums.length;
 };
-
-let input = [
-  // TC: 1
-  [1, 1, 2],
-  // TC: 2
-  [0, 0, 1, 1, 1, 2, 2, 3, 3, 4],
-
-  [0, 1, 2, 2, 3, 4, 5, 5, 7, 7, 7, 8, 8, 9, 9, 9],
-];
-
-for (let i = 0; i < input.length; i++) {
-  process.stdout.write(`#${i + 1} `);
-  console.log(solution(input[i]));
-}
