76290445b52aed42ee1a98285eb4b86f533c3a01
SleeplessN <gmlwns7020@naver.com>
2022-06-09 01:25:31 +0900

    Delete example and add question url

--- leetcode/714. Best Time to Buy and Sell Stock with Transaction Fee.js
+++ leetcode/714. Best Time to Buy and Sell Stock with Transaction Fee.js
@@ -1,21 +1,16 @@
-function solution(prices, fee) {
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
+
+/**
+ * @param {number[]} prices
+ * @param {number} fee
+ * @return {number}
+ */
+var maxProfit = function (prices, fee) {
   let cash = 0;
   let hold = -prices[0];
   for (let i = 1; i < prices.length; i++) {
     cash = Math.max(cash, hold + prices[i] - fee);
     hold = Math.max(hold, cash - prices[i]);
-    console.log(`cash : ${cash} / hold : ${hold}`);
   }
   return cash;
-}
-
-let input = [
-  // TC: 1
-  [[1, 3, 2, 8, 4, 9], 2],
-  // TC: 2
-  [[1, 3, 7, 5, 10, 3], 3],
-];
-
-for (let i = 0; i < input.length; i++) {
-  console.log(`#${i + 1} ${solution(input[i][0], input[i][1])}`);
-}
+};
