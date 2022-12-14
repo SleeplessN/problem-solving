785d97dee7915968479ad94771f07deae303de3c
SleeplessN <gmlwns7020@naver.com>
2022-12-14 17:24:09 +0900

    Upload my solution on 12/14

---
+++ programmers/땅따먹기.js
@@ -0,0 +1,26 @@
+function solution(land) {
+  let result = 0;
+
+  for (i = 1; i < land.length; i++) {
+    land[i][0] += Math.max(land[i - 1][1], land[i - 1][2], land[i - 1][3]);
+    land[i][1] += Math.max(land[i - 1][0], land[i - 1][2], land[i - 1][3]);
+    land[i][2] += Math.max(land[i - 1][0], land[i - 1][1], land[i - 1][3]);
+    land[i][3] += Math.max(land[i - 1][0], land[i - 1][1], land[i - 1][2]);
+  }
+  result = land[land.length - 1];
+  return Math.max(...result);
+}
+
+let input = [
+  // TC: 1
+  [
+    [1, 2, 3, 5],
+    [5, 6, 7, 8],
+    [4, 3, 2, 1],
+  ],
+];
+
+for (let i = 0; i < input.length; i++) {
+  process.stdout.write(`#${i + 1} `);
+  console.log(solution(input[i]));
+}
