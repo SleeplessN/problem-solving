924bd7e69b7b5086181ad8b9030a6619cfd659ba
SleeplessN <gmlwns7020@naver.com>
2022-12-27 16:15:08 +0900

    Upload my solution on 12/27

---
+++ programmers/삼각 달팽이.js
@@ -0,0 +1,29 @@
+function solution(n) {
+  const answer = new Array(n).fill().map((_, i) => new Array(i + 1));
+
+  let count = 0;
+  let x = -1;
+  let y = 0;
+  while (n > 0) {
+    for (let i = 0; i < n; i++) answer[++x][y] = ++count;
+    for (let i = 0; i < n - 1; i++) answer[x][++y] = ++count;
+    for (let i = 0; i < n - 2; i++) answer[--x][--y] = ++count;
+
+    n -= 3;
+  }
+  return answer.flatMap((e) => e);
+}
+
+let input = [
+  // TC: 1
+  4,
+  // TC: 2
+  5,
+  // TC: 3
+  6,
+];
+
+for (let i = 0; i < input.length; i++) {
+  process.stdout.write(`#${i + 1} `);
+  console.log(solution(input[i]));
+}
